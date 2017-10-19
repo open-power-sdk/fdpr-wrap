e32f9e6848c4b180f0d28b68d72bf49f6ba877cb
Genady Gurevich <genadyg@il.ibm.com>
2017-10-20 00:30:04 +0300

    fixed scripts, moving files to folders

--- bin/fdpr_dl.py
+++ bin/fdpr_dl.py
@@ -14,9 +14,7 @@ try:
    fdpr_file = os.environ["FDPR_BINDIR"] + "/fdprpro"
 except KeyError: 
    fdpr_file = "./fdprpro"
-#print   fdpr_file
   
-
 if not os.path.isfile(fdpr_file):
     print 'define FDPR_BINDIR as path to fdprpro'
     sys.exit()
@@ -41,9 +39,8 @@ if (len(argv_list) < 2):
   print 'fdpr_dl.py <fdpr options> '
   sys.exit()
 del argv_list[0]
-#print argv_list
 fdpr_params = " ".join(argv_list)
-# print fdpr_params
+
 
 #analyze parameters
 def findIndexByParameter(str):
@@ -71,8 +68,6 @@ try:
 except KeyError: 
    output_path = bin_name_path
 	
-#print bin_name
-
 a_index = findIndexByParameter("-a") 
 if a_index < 0 or len(argv_list) <= a_index or argv_list[a_index + 1] not in ['opt', 'instr']:	
   print 'fdpr parameters error (action)'
@@ -89,10 +84,9 @@ if len(argv_list) <= max([f_index, o_index, j_index, fd_index]) + 1:
    
 ext = ''
 ext_lib = ''
-instr_output_path = output_path + "Instr/"
-opt_output_path  = output_path + "Opt/"
+instr_output_path = output_path + "instr/"
+opt_output_path  = output_path + "opt/"
 
-#print argv_list[a_index + 1]
 if (argv_list[a_index + 1].strip() == 'opt'):
    ext = ".fdpr"
    ext_lib 
@@ -109,44 +103,25 @@ def ensure_dir(file_path):
 		
 ensure_dir(output_path) 
 
-
-
-#print ext  
-#find libraries
-
-ld_lib_path = os.environ.get('LD_LIBRARY_PATH')
-lpathes = ld_lib_path.split(":");
-lpathes.append(bin_name_path)
-#print lpathes
 lddOut = subprocess.check_output(['ldd', bin_name])
-#print lddOut  
+
 libraries = []
 for line in lddOut.splitlines():
     match = re.match(r'\t(.*) => (.*) \(0x', line)
     if match and match.group(2):
-       libraries.append(match.group(2))
-    else:
-      for lpath in lpathes:
-        lfull_name = lpath  + line.split()[0]
-        if os.path.isfile(lfull_name):
-           libraries.append(lfull_name)	
-           break
+	   path = os.path.realpath(match.group(2))
+	   #print match.group(2) + " " + path
+	   libraries.append(path)
 
-# print libraries
-
-def ExecuteFdpr():
-   print  command_line
+def ExecuteCmd():
    result = os.system(command_line)
-   result_str = "  ERROR";
+   #print command_line + " " + str(result)
    if result == 0:
-      result_str = "  SUCCESS"
-   print "result = "  + result_str
+	  print  command_line + ": SUCCESS"
    return result
 
-fd_instr = 10;
-fd_count = 0  
+fd_instr = 999;
  
-   
 # call fdpr for original binary
 if fd_index < 0 and ext == ".instr":    
     argv_list.append("-fd") 
@@ -155,13 +130,14 @@ if fd_index < 0 and ext == ".instr":
 fdpr_params = " ".join(argv_list)
 log_name = bin_name + ext + ".log" 
 command_line = fdpr_file + " " + fdpr_params + " >" + log_name + " 2>&1"
-ExecuteFdpr()
+ExecuteCmd()
 
 # call  fdpr 	for each library
 lib_args = argv_list
 fd_index = findIndexByParameter("-fd")
+orig_ext = ".orig"  
 
-def ReplaceFdprParam(fd_count):
+for library in libraries:
   if p_index == -1:
      lib_args[-1] = library
   else:
@@ -171,37 +147,18 @@ def ReplaceFdprParam(fd_count):
   if o_index >= 0:	 
      lib_args[o_index + 1] = output_path + os.path.basename(library) + ext_lib
   if j_index >= 0: 	 
-     lib_args[j_index + 1] = output_path + os.path.basename(library) + ".j.xml"  
-  fd_count = fd_count + 1 
-  if fd_index >= 0:
-     fd_instr = int(lib_args[fd_index + 1]) 
-     if fd_instr + 1 > 1000:
-        fd_instr = 10
-     lib_args[fd_index + 1] = str(fd_instr + 1)
-  #else:
- #    if ext == ".instr":
- #      lib_args.append("-fd") 
- #      lib_args.append(str(fd_instr + fd_count))
+     lib_args[j_index + 1] = output_path + os.path.basename(library) + ".j.xml"
 	 
-def CreateCommandLine():
   log_name = output_path + os.path.basename(library) + ".log" 
   fdpr_params = " ".join(lib_args)
-  command_line = fdpr_file + " " + fdpr_params + " >" + log_name + " 2>&1" 
-  return command_line
-
-orig_ext = ".origf"  
-for library in libraries:
-  ReplaceFdprParam(fd_count)   
-  command_line = CreateCommandLine()
+  command_line = fdpr_file + " " + fdpr_params + " >" + log_name + " 2>&1"
   # replace libraries with original
- # if ext == ".fdpr":
   orig_lib_name = library + orig_ext
   if os.path.isfile(orig_lib_name):
      sf_command = "ln -sf " + orig_lib_name + " " + library
-     print sf_command
      os.system(sf_command)
-     
-  result = ExecuteFdpr()
+ 
+  result = ExecuteCmd()
    
   if result == 0: 
     if ext == ".instr" and os.path.isfile(output_path + os.path.basename(library) + ".prof"): 
@@ -209,11 +166,9 @@ for library in libraries:
 	   os.system(copy_command)
     if not os.path.isfile(library + orig_ext):
        copy_command = "cp " + library + " " + library + orig_ext
-       print copy_command
        os.system(copy_command)
     if ext == ".instr":
        sf_command = "ln -sf " + lib_args[o_index + 1] + " " + library
-       print sf_command
        os.system(sf_command)
 	
 	
